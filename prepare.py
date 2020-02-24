e45a5e159e2d495529ecaefa86e06f29b0a57def
Tosin Akinosho <tosin.akinosho@gmail.com>
2020-02-24 18:23:28 -0500

    adding default network path

--- prepare.py
+++ prepare.py
@@ -234,6 +234,7 @@ def main():
                 and "create_iso" in node
                 and not node["create_iso"]
             ):
+                path = tmppath + data["paths"]["network"]
                 CreateNetworkFiles(path, template, env, node)
             elif template == "templateIsolinux" or template == "templateEFI":
                 if (
